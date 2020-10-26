ce0ccff0c2ef196cedaef1367b0e59537e01bf35
Jenna Chestnut <jennabot5000@gmail.com>
2020-10-26 15:18:08 -0500

    fully functional

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const getParks = (apiKey, state, max) => {
+    fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=${max}&api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(responseJson => displayParks(responseJson))
+    .catch(error => alert(error));
+}
+
+const displayParks = (responseJson) => {
+    $('.parkslist').html("");
+    let results = "";
+    responseJson.data.forEach(el => {
+    let add = el.addresses[0];
+    results += `<h2 class="fullname">${el.fullName}</h2>
+        <img src="${el.images[0].url}" alt="parkpic">
+        <p class="description">${el.description}</p>
+        <p class="addresses">Address:
+        ${add.line1}, ${add.city} ${add.stateCode} ${add.postalCode}</p>
+        <p class="url"><a href="${el.url}">Click here to learn more!</a></p>`
+    })
+    $('.parkslist').append(results);
+}
+
+
+const handleClick = () => {  
+    $('main').on('submit', event => {
+    event.preventDefault();
+    console.log('button works!');
+    let max = $('#max').val();
+    let state = $('#state').val();
+    console.log(max, state)
+    getParks(apiKey(), state, max)
+    })
+}
+
+const apiKey = () => {
+return 'ZKKW2iUmS3v5eYduweLz1wgXVZiiVFZfiCbxvLEi';
+}
+
+const main = () => {
+    handleClick();
+    console.log('app is live!!');
+}
+
+$(main)
